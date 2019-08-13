3de39f66ceead19305fc9ba1200a8ce6f35e972d
yaoyu bao <yaobao5354@gmail.com>
2019-08-13 21:09:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,5 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index)
-  else if   
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
